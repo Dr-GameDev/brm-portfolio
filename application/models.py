7db516fa1d47720dc0d13bdb7081cd4f6d9c300d
Babalo Majiyezi <80054866+Dr-GameDev@users.noreply.github.com>
2024-07-01 15:31:03 +0200

    Fix: remove all unnecessary files

--- application/models.py
+++
@@ -1,8 +0,0 @@
-from application import db
-
-class Client(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
-    subject = db.Column(db.String(255), nullable=False)
-    message = db.Column(db.String(400), nullable=False)
